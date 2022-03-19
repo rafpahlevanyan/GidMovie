f6a30b8ccd20bc7d26dae61b07af131ef75d5e8b
rafpahlevanyan <rafok3nt@gmail.com>
2022-03-20 00:40:14 +0400

    Initial commit

---
+++ src/main/java/com/example/gidmovie/GidMovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.gidmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GidMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GidMovieApplication.class, args);
+    }
+
+}
