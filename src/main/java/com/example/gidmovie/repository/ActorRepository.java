ff3c8e1a999826dbf12bd8c5ac5fe14d840ab5fe
rafpahlevanyan <91832538+rafpahlevanyan@users.noreply.github.com>
2022-04-08 02:56:27 +0400

    added Category & Movie Services , Movie Controller

--- src/main/java/com/example/gidmovie/repository/ActorRepository.java
+++ src/main/java/com/example/gidmovie/repository/ActorRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface ActorRepository extends JpaRepository<Actor, Integer> {
     List<User> findAllByName(String name);
 
+
+
 }
--- src/main/java/com/example/gidmovie/service/ActorService.java
+++ src/main/java/com/example/gidmovie/service/ActorService.java
@@ -1,9 +1,20 @@
 package com.example.gidmovie.service;
 
+import com.example.gidmovie.entity.Actor;
+import com.example.gidmovie.repository.ActorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ActorService {
+
+    private final ActorRepository actorRepository;
+
+    public List<Actor> findAll(){
+        return actorRepository.findAll();
+    }
+
 }
