ffaa6277686fd7babf52b3b5b934390b0b4f2f1a
rafpahlevanyan <91832538+rafpahlevanyan@users.noreply.github.com>
2022-04-08 02:56:44 +0400

    added Category & Movie Services , Movie Controller

---
+++ src/main/java/com/example/gidmovie/service/MovieService.java
@@ -0,0 +1,44 @@
+package com.example.gidmovie.service;
+
+import com.example.gidmovie.entity.Actor;
+import com.example.gidmovie.entity.Movie;
+import com.example.gidmovie.repository.ActorRepository;
+import com.example.gidmovie.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+    private final ActorRepository actorRepository;
+    public Movie getById(int id){
+        return movieRepository.getById(id);
+    }
+
+    public Page<Movie> findAll(Pageable pageable) {
+        return movieRepository.findAll(pageable);
+    }
+
+    public Movie addMovie(Movie movie,List<Integer> actors) {
+        List<Actor> actorsFromRequest = getActorsFromRequest(actors);
+        movie.setActors(actorsFromRequest);
+        movieRepository.save(movie);
+        return movie;
+    }
+
+    private List<Actor> getActorsFromRequest(List<Integer> actorsIds) {
+        List<Actor> actors = new ArrayList<>();
+        for (Integer actor : actorsIds) {
+            actors.add(actorRepository.getById(actor));
+        }
+        return actors;
+    }
+
+}
